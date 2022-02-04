20b02b880b83b6bbbba6b22ec42823b6f7cb922a
john <johnadexzy@gmail.com>
2022-02-04 13:19:57 +0100

    updated projects

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -2,8 +2,23 @@ import calculator from './img/calculator.png';
 import hosterr from './img/hosterr.png';
 import todo from './img/todo.png';
 import unifeed from './img/unifeed.png';
+import digipay from './img/digipay.png';
 
 const projectItems = [
+  {
+    image: digipay,
+    p: 'A web wallet system using firebase as backend (React)',
+    href1: 'https://digipay-nu.vercel.app/',
+    href2: 'https://github.com/adexzy1/Digipay',
+    alt: 'digipay',
+  },
+  {
+    image: unifeed,
+    p: 'A simple landing page website that Uses the dev.to Api to get the latest post of a specific user',
+    href1: 'https://unifeed.vercel.app/',
+    href2: 'https://github.com/adexzy1/unifeed',
+    alt: 'unifeed',
+  },
   {
     image: calculator,
     p: 'A beautiful and functional calculator app built with HTML, CSS and JS',
@@ -13,24 +28,17 @@ const projectItems = [
   },
   {
     image: hosterr,
-    p: 'A beautiful and functional calculator app built with HTML, CSS and JS',
-    href1: 'https://calculator-app-mocha.vercel.app/',
-    href2: 'https://github.com/adexzy1/calculator-App',
+    p: 'A functional landing page with a simple animation on in the hero image',
+    href1: 'https://hosterr-waitlist.vercel.app/',
+    href2: 'https://github.com/adexzy1/Hosterr-waitlist',
     alt: 'hosterr',
   },
   {
     image: todo,
-    p: 'A beautiful and functional calculator app built with HTML, CSS and JS',
-    href1: 'https://calculator-app-mocha.vercel.app/',
-    href2: 'https://github.com/adexzy1/calculator-App',
+    p: 'A functional todo app with different functionalities such as Adding todo, deleting todos, marking todos as completed',
+    href1: 'https://todo-app-chi-nine.vercel.app/',
+    href2: 'https://github.com/adexzy1/Todo-app',
     alt: 'todo',
-  },
-  {
-    image: unifeed,
-    p: 'A beautiful and functional calculator app built with HTML, CSS and JS',
-    href1: 'https://calculator-app-mocha.vercel.app/',
-    href2: 'https://github.com/adexzy1/calculator-App',
-    alt: 'unifeed',
   },
 ];
 
